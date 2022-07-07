cd861d9ec531151c00c5242df17951dd15fdf347
Amanjit313 <amanjitsingh313@gmail.com>
2022-07-07 15:47:30 +0200

    delete

--- resources/views/admin/pizza/index.blade.php
+++ resources/views/admin/pizza/index.blade.php
@@ -20,6 +20,18 @@
                 <td class="d-flex">
                     <a class="btn btn-success" href="{{ route('admin.pizzas.show', $pizza) }}">SHOW</a>
                     <a class="btn btn-primary mx-2" href="{{ route('admin.pizzas.edit', $pizza) }}">EDIT</a>
+
+                    <form
+                            action="{{ route('admin.pizzas.destroy', $pizza) }}"
+                            method="POST"
+                            onsubmit="return confirm('Confermi di eliminare la Pizza {{ $pizza->nome }}?')">
+
+                        @csrf
+                        @method('DELETE')
+                        <button type="sumbit" class="btn btn-danger">DELETE</button>
+
+                    </form>
+
                 </td>
             </tr>
         @endforeach
